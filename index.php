bbd7b9e5a451102aad8760104518691ba2adfcff
dji22 <119694873+dji22@users.noreply.github.com>
2022-12-03 10:15:25 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,23 @@
-<?php echo '<p>Bonjour le monde</p>'; ?>
+<?php 
+// index.php
+session_start();
+
+// Si l'utilisateur est connecté, on récupère son identité via la session.
+$identity = null;
+if (isset($_SESSION['identity'])) {
+    $identity = $_SESSION['identity'];
+}
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Page d'accueil</title>
+    </head>
+    <body>
+        <h1>Page d'accueil</h1>
+        <?php if ($identity==null): ?>
+        <a href="login.php">Connexion</a>
+        <?php else: ?>
+        <strong>Bienvenue, <?= $identity ?></strong> <a href="logout.php">Déconnexion</a>
 
